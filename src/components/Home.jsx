a5cd139f3e8ef04deb56f05ce21e0f03663de93e
OsvaldoSilveira <oasosvaldo@gmail.com>
2023-12-20 10:51:15 -0300

    Atualizando

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,49 +1,43 @@
 import React from 'react'
 import './Pages.css'
-import imgJaquetaCouro from '../imgs/jaqueta_couro.jpg'
-import imgMoletomFem from '../imgs/conjunto_moleton.jpg'
-import imgCalcaJeansAzul from '../imgs/calça_jeans.jpg'
-import imgBiquiniCroucheBege from '../imgs/biquine_crouche.jpg'
+import './Home.css'
+import img1 from '../imgs-slider/calca_fleir_azul_marinho_laço.jpg'
+import img2 from '../imgs-slider/calca_fleir_azul_claro_desbotada.jpg'
+import img3 from '../imgs-slider/calca_fleir_azul_marinho.jpg'
+import img4 from '../imgs-slider/calca_fleir_azul_marinho2.jpg'
 
 const Home = () => {
-  return (
+
+    const box = document.querySelector(".container")
+    const imagens = document.querySelectorAll(".container img")
+
+    let contador = 0
+
+    function slider() {
+        contador++
+
+        if (contador > imagens.length - 1) {
+            contador = 0
+        }
+
+        box.style.transform = `translateX(${-contador * 700}px)`
+
+    }
+
+    setInterval(slider, 2000)
+
+    return (
     <div className='Pag'>
       <h2>Bem vindo a nossa loja!</h2>
         <h3>Lançamentos</h3>
-            <section className="produtos">
-                <ul className="galeriaProdutos">
-                    <li className="detalheProduto">
-                        <h6>Jaqueta Couro</h6>
-                        <li className="imgProduto"><img src={imgJaquetaCouro} alt="" /></li>
-                        <li className="bodyProduto">Jaqueta em couro sintético masculina</li>
-                        <li className="bodyProduto">Preço: R$ 320,00</li>
-                    </li>
-                </ul>
-                <ul className="galeriaProdutos">
-                    <li className="detalheProduto">
-                        <li><h6>Conjunto Moletom</h6></li>
-                        <li className="imgProduto"><img src={imgMoletomFem} alt="" /></li>
-                        <li className="bodyProduto">Conjunto de moletom feminino</li>
-                        <li className="bodyProduto">Preço: R$ 120,00</li>
-                    </li>
-                </ul>
-                <ul className="galeriaProdutos">
-                    <li className="detalheProduto">
-                        <h6>Calça Jeans Azul</h6>
-                        <li className="imgProduto"><img src={imgCalcaJeansAzul} alt="" /></li>
-                        <li className="bodyProduto">Calça jeans azul feminina</li>
-                        <li className="bodyProduto">Preço: R$ 140,00</li>
-                    </li>
-                </ul>
-                <ul className="galeriaProdutos">
-                    <li className="detalheProduto">
-                        <h6>Biquine Crouche</h6>
-                        <li className="imgProduto"><img src={imgBiquiniCroucheBege} alt="" /></li>
-                        <li className="bodyProduto">Biquini em crouche bege</li>
-                        <li className="bodyProduto">Preço: R$ 320,00</li>
-                    </li>
-                </ul>
-            </section>
+            <div className="carrossel">
+                <div className="container">
+                    <img className='img-carrossel' src={img1} alt="Calça Flair" />
+                    <img className='img-carrossel' src={img2} alt="Calça Flair" />
+                    <img className='img-carrossel' src={img3} alt="Calça Flair" />
+                    <img className='img-carrossel' src={img4} alt="Calça Flair" />
+                </div>
+            </div>
     </div>
   )
 }
