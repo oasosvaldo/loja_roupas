e1f9ce76738ad124b28007a87cbd106252179a5a
oasosvaldo <oasosvaldo@gmail.com>
2024-01-14 07:29:25 -0300

    update

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -7,33 +7,10 @@ import img3 from '../imgs-slider/calca_fleir_azul_marinho.jpg'
 
 const Home = () => {
 
-  let time = 2000,
-    currentImageIndex = 0,
-    images = document.querySelectorAll(".slider .center"),
-    max = images.length;
-
-  function nextImage() {
-
-    images[currentImageIndex]
-    .classList.remove("foco")
-
-    currentImageIndex++
-
-    if(currentImageIndex >= max)
-    currentImageIndex = 0
-
-    images[currentImageIndex]
-    .classList.add("foco")
-  }
-
-  function start() {
-    setInterval(() => {
-      nextImage()
-    }, time)
-  }
+  
 
   return (
-    <div onLoad={start()} className='Pag'>
+    <div className='Pag'>
 
       <h2>Bem vindo a nossa loja!</h2>
       <h3>Lançamentos</h3>
